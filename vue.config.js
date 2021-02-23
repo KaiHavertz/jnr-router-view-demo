c16418d743cfd57bc89ed6047b05a998a47431ca
jnr <13692453351@163.com>
2021-02-23 21:17:01 +0800

    应用 router-view等的小demo

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
-const webpack = require("webpack");
+//const webpack = require("webpack");
 
 module.exports = {
   publicPath: "./", // 打包设置
-  chainWebpack: config => {
-    config.plugin("provide").use(webpack.ProvidePlugin, [
-      {
-        $: "jquery",
-        jquery: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery"
-      }
-    ]);
-  }
+  // chainWebpack: config => {
+  //   config.plugin("provide").use(webpack.ProvidePlugin, [
+  //     {
+  //       $: "jquery",
+  //       jquery: "jquery",
+  //       jQuery: "jquery",
+  //       "window.jQuery": "jquery"
+  //     }
+  //   ]);
+  // }
 };
